0631501b542ff1b3cbb38e713c31a26c62057b96
GöktürkTonay <gokturktonay@gmail.com>
2025-09-28 15:07:26 +0300

    Basic Flappy Bird game implemented.
    - Bird can jump
    - Pipes appear at random heights
    - Score system and collision detection added
    - Game over screen and start button implemented

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pipeTop1 = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipeBottom1 = new System.Windows.Forms.PictureBox();
+            this.pipeBottom2 = new System.Windows.Forms.PictureBox();
+            this.pipeTop2 = new System.Windows.Forms.PictureBox();
+            this.pipeBottom3 = new System.Windows.Forms.PictureBox();
+            this.pipeTop3 = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.gameOverBox = new System.Windows.Forms.PictureBox();
+            this.startLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameOverBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            // 
+            // pipeTop1
+            // 
+            this.pipeTop1.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeTop1.Location = new System.Drawing.Point(231, -319);
+            this.pipeTop1.Name = "pipeTop1";
+            this.pipeTop1.Size = new System.Drawing.Size(63, 502);
+            this.pipeTop1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop1.TabIndex = 1;
+            this.pipeTop1.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.SystemColors.Menu;
+            this.bird.Image = global::WindowsFormsApp1.Properties.Resources.yellowbird_midflap;
+            this.bird.Location = new System.Drawing.Point(43, 115);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(53, 38);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // pipeBottom1
+            // 
+            this.pipeBottom1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pipeBottom1.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBottom1.Location = new System.Drawing.Point(231, 317);
+            this.pipeBottom1.Name = "pipeBottom1";
+            this.pipeBottom1.Size = new System.Drawing.Size(63, 502);
+            this.pipeBottom1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom1.TabIndex = 5;
+            this.pipeBottom1.TabStop = false;
+            this.pipeBottom1.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pipeBottom2
+            // 
+            this.pipeBottom2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pipeBottom2.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBottom2.Location = new System.Drawing.Point(482, 317);
+            this.pipeBottom2.Name = "pipeBottom2";
+            this.pipeBottom2.Size = new System.Drawing.Size(63, 502);
+            this.pipeBottom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom2.TabIndex = 8;
+            this.pipeBottom2.TabStop = false;
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeTop2.Location = new System.Drawing.Point(482, -319);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Size = new System.Drawing.Size(63, 502);
+            this.pipeTop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop2.TabIndex = 7;
+            this.pipeTop2.TabStop = false;
+            // 
+            // pipeBottom3
+            // 
+            this.pipeBottom3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pipeBottom3.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBottom3.Location = new System.Drawing.Point(625, 317);
+            this.pipeBottom3.Name = "pipeBottom3";
+            this.pipeBottom3.Size = new System.Drawing.Size(63, 502);
+            this.pipeBottom3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom3.TabIndex = 13;
+            this.pipeBottom3.TabStop = false;
+            // 
+            // pipeTop3
+            // 
+            this.pipeTop3.Image = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeTop3.Location = new System.Drawing.Point(625, -319);
+            this.pipeTop3.Name = "pipeTop3";
+            this.pipeTop3.Size = new System.Drawing.Size(63, 502);
+            this.pipeTop3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop3.TabIndex = 12;
+            this.pipeTop3.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::WindowsFormsApp1.Properties.Resources._base;
+            this.ground.Location = new System.Drawing.Point(-7, 484);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(712, 182);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 14;
+            this.ground.TabStop = false;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft PhagsPa", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(275, 33);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(162, 50);
+            this.scoreLabel.TabIndex = 15;
+            this.scoreLabel.Text = "Score: 0";
+            // 
+            // gameOverBox
+            // 
+            this.gameOverBox.Image = global::WindowsFormsApp1.Properties.Resources.gameover;
+            this.gameOverBox.Location = new System.Drawing.Point(149, 210);
+            this.gameOverBox.Name = "gameOverBox";
+            this.gameOverBox.Size = new System.Drawing.Size(413, 82);
+            this.gameOverBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gameOverBox.TabIndex = 16;
+            this.gameOverBox.TabStop = false;
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.startLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.startLabel.Location = new System.Drawing.Point(148, 350);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(169, 76);
+            this.startLabel.TabIndex = 17;
+            this.startLabel.Text = "Start";
+            this.startLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.background_day;
+            this.ClientSize = new System.Drawing.Size(700, 597);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.gameOverBox);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom3);
+            this.Controls.Add(this.pipeTop3);
+            this.Controls.Add(this.pipeBottom2);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.pipeBottom1);
+            this.Controls.Add(this.pipeTop1);
+            this.Controls.Add(this.bird);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameOverBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox pipeTop1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pipeBottom1;
+        private System.Windows.Forms.PictureBox pipeBottom2;
+        private System.Windows.Forms.PictureBox pipeTop2;
+        private System.Windows.Forms.PictureBox pipeBottom3;
+        private System.Windows.Forms.PictureBox pipeTop3;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.PictureBox gameOverBox;
+        private System.Windows.Forms.Label startLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        int gravity = 2;         // Kuşun düşme hızı
+        int jumpForce = -15;      // Space basınca yukarı hareket
+        int pipeSpeed = 8;       // Boruların hızı
+        int score = 0;
+        int birdSpeed = 0;     // Kuşun anlık hızı
+        int maxFallSpeed = 6;
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            gameTimer.Stop(); // oyun başlamasın
+            startLabel.Visible = true; // start yazısı görünsün
+
+            gameOverBox.BackColor = Color.Transparent;
+            bird.BackColor = Color.Transparent;
+            pipeTop1.BackColor = Color.Transparent;
+            pipeBottom1.BackColor = Color.Transparent;
+            pipeTop2.BackColor = Color.Transparent;
+            pipeBottom2.BackColor = Color.Transparent;
+            pipeTop3.BackColor = Color.Transparent;
+            pipeBottom3.BackColor = Color.Transparent;
+            scoreLabel.BackColor = Color.Transparent;
+
+            gameOverBox.Visible = false;
+
+            gameTimer.Tick += GameTimerEvent;
+            this.KeyDown += KeyIsDown;
+            this.KeyUp += KeyIsUp;
+
+            // pipeTop'u ters çevir
+            pipeTop1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pipeTop2.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pipeTop3.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+
+            // Boruların başlangıç konumu
+            ResetPipes(pipeTop1, pipeBottom1);
+            ResetPipes(pipeTop2, pipeBottom2);
+            ResetPipes(pipeTop3, pipeBottom3);
+
+            // İkinci boru setini biraz daha ileriye kaydır
+            pipeTop2.Left += 230;
+            pipeBottom2.Left += 230;
+
+            pipeTop3.Left += 450;
+            pipeBottom3.Left += 450;
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            // Kuşu aşağı doğru hareket ettir
+            birdSpeed += gravity;
+
+            // Maksimum düşme hızını aşma
+            if (birdSpeed > maxFallSpeed)
+                birdSpeed = maxFallSpeed;
+
+            // Kuşun konumunu anlık hızına göre değiştir
+            bird.Top += birdSpeed;
+
+            // Boruları sola kaydır
+            pipeBottom1.Left -= pipeSpeed;
+            pipeTop1.Left -= pipeSpeed;
+            pipeBottom2.Left -= pipeSpeed;
+            pipeTop2.Left -= pipeSpeed;
+            pipeBottom3.Left -= pipeSpeed;
+            pipeTop3.Left -= pipeSpeed;
+
+            // Eğer borular ekran dışına çıkarsa tekrar sağdan getir
+            if (pipeBottom1.Left < -pipeBottom1.Width)
+            {
+                ResetPipes(pipeTop1, pipeBottom1);
+                score++;
+                scoreLabel.Text = "Score: " + score;
+            }
+
+            if (pipeTop2.Left < -pipeTop2.Width)
+            {
+                ResetPipes(pipeTop2, pipeBottom2);
+                score++;
+                scoreLabel.Text = "Score: " + score;
+            }
+
+            if (pipeTop3.Left < -pipeTop3.Width)
+            {
+                ResetPipes(pipeTop3, pipeBottom3);
+                score++;
+                scoreLabel.Text = "Score: " + score;
+            }
+
+            // Çarpışma kontrolü
+            if (bird.Bounds.IntersectsWith(pipeBottom1.Bounds) ||
+                bird.Bounds.IntersectsWith(pipeTop1.Bounds) ||
+                bird.Bounds.IntersectsWith(ground.Bounds) ||
+                bird.Top < -25)
+            {
+                EndGame();
+            }
+        }
+
+        private void ResetGame()
+        {
+            gameTimer.Stop();
+
+            score = 0;
+            scoreLabel.Text = "Score: " + score;
+
+            bird.Top = this.ClientSize.Height / 2;
+
+            ResetPipes(pipeTop1, pipeBottom1);
+            ResetPipes(pipeTop2, pipeBottom2);
+            ResetPipes(pipeTop3, pipeBottom3);
+
+            pipeTop2.Left += 230;
+            pipeBottom2.Left += 230;
+            pipeTop3.Left += 450;
+            pipeBottom3.Left += 450;
+
+            gameOverBox.Visible = false;
+            startLabel.Visible = false;
+
+            gameTimer.Start();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                birdSpeed = jumpForce; // Space basıldığında yukarı çıkar
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                // Space bırakıldığında tekrar düşer
+            }
+        }
+
+        private void ResetPipes(PictureBox topPipe, PictureBox bottomPipe)
+        {
+            int gap = 150; // Üst ve alt boru arası boşluk
+            int minY = 4;
+            int maxY = this.ClientSize.Height - ground.Height - gap - 10;
+
+            int pipeTopY = rand.Next(minY, maxY);
+
+            topPipe.Top = pipeTopY - topPipe.Height;
+            bottomPipe.Top = pipeTopY + gap;
+
+            // Bu boru setini ekranın en sağından biraz daha ileri başlat
+            topPipe.Left = this.ClientSize.Width + 100;
+            bottomPipe.Left = this.ClientSize.Width + 100;
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            gameOverBox.Visible = true;
+            startLabel.Visible = true; // yeniden başlatma butonu gibi görünür
+            startLabel.Text = "PLAY AGAIN";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            startLabel.Visible = false; // start yazısını gizle
+            ResetGame(); // oyun nesnelerini başa sar
+            gameTimer.Start(); // oyun başlasın
+        }
+    }
+}
